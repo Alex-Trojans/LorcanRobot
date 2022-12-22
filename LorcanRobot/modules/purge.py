f29c50773ff4d9249a7f7bb3dde7b5e10bf334f2
EDWARD ELRIC <107063122+EDWARD-ELRIC39@users.noreply.github.com>
2022-12-22 20:33:06 +0530

    Update purge.py

--- LorcanRobot/modules/purge.py
+++ LorcanRobot/modules/purge.py
@@ -21,16 +21,16 @@ async def purge_messages(event):
         )
         and event.from_id not in [1087968824]
     ):
-        await event.reply("Only Admins are allowed to use this command")
+        await event.reply("Oɴʟʏ Aᴅᴍɪɴs Aʀᴇ Aʟʟᴏᴡᴇᴅ Tᴏ Usᴇ Tʜɪs Cᴏᴍᴍᴀɴᴅ")
         return
 
     if not await can_delete_messages(message=event):
-        await event.reply("Can't seem to purge the message")
+        await event.reply("Cᴀɴ'ᴛ Sᴇᴇᴍ Tᴏ Pᴜʀɢᴇ Tʜᴇ Mᴇssᴀɢᴇ")
         return
 
     reply_msg = await event.get_reply_message()
     if not reply_msg:
-        await event.reply("Reply to a message to select where to start purging from.")
+        await event.reply("Rᴇᴘʟʏ Tᴏ A Mᴇssᴀɢᴇ Tᴏ Sᴇʟᴇᴄᴛ Wʜᴇʀᴇ Tᴏ Sᴛᴀʀᴛ Pᴜʀɢɪɴɢ Fʀᴏᴍ.")
         return
     messages = []
     message_id = reply_msg.id
@@ -48,7 +48,7 @@ async def purge_messages(event):
     except:
         pass
     time_ = time.perf_counter() - start
-    text = f"Purged Successfully in {time_:0.2f} Second(s)"
+    text = f"Pᴜʀɢᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ Iɴ {time_:0.2f} Sᴇᴄᴏɴᴅ(s)"
     await event.respond(text, parse_mode="markdown")
 
 async def delete_messages(event):
@@ -62,16 +62,16 @@ async def delete_messages(event):
         )
         and event.from_id not in [1087968824]
     ):
-        await event.reply("Only Admins are allowed to use this command")
+        await event.reply("Oɴʟʏ Aᴅᴍɪɴs Aʀᴇ Aʟʟᴏᴡᴇᴅ Tᴏ Usᴇ Tʜɪs Cᴏᴍᴍᴀɴᴅ")
         return
 
     if not await can_delete_messages(message=event):
-        await event.reply("Can't seem to delete this?")
+        await event.reply("Cᴀɴ'ᴛ Sᴇᴇᴍ Tᴏ Dᴇʟᴇᴛᴇ Tʜɪs?")
         return
 
     message = await event.get_reply_message()
     if not message:
-        await event.reply("Whadya want to delete?")
+        await event.reply("Wʜᴀᴅʏᴀ Wᴀɴᴛ Tᴏ Dᴇʟᴇᴛᴇ?")
         return
     chat = await event.get_input_chat()
     del_message = [message, event.message]
