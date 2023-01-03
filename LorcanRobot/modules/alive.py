64b17980a5dcf8e25b41d34238aa681e60260a6d
EDWARD ELRIC <107063122+EDWARD-ELRIC39@users.noreply.github.com>
2023-01-03 10:53:47 +0530

    Update alive.py

--- LorcanRobot/modules/alive.py
+++ LorcanRobot/modules/alive.py
@@ -9,16 +9,16 @@ from LorcanRobot.events import register
 from LorcanRobot import telethn as tbot, BOT_USERNAME
 
 
-PHOTO = "https://telegra.ph/file/51aa7cab7dace139d82ed.jpg"
+PHOTO = "https://telegra.ph/file/1c1d3914a86a96a363b44.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**Hᴇʏ [{event.sender.first_name}](tg://user?id={event.sender.id}), I'ᴍ Lᴏʀᴄᴀɴ Rᴏʙᴏᴛ.** \n\n"
-  TEXT += " **I'ᴍ Wᴏʀᴋɪɴɢ Pʀᴏᴘᴇʀʟʏ** \n\n"
+  TEXT += " **✪ I'ᴍ Wᴏʀᴋɪɴɢ Pʀᴏᴘᴇʀʟʏ ✪** \n\n"
   TEXT += f"❂ **Mʏ Mᴀsᴛᴇʀ  : [ᴇᴅᴡᴀʀᴅ ᴇʟʀɪᴄ ](https://t.me/lI_EDWARD_Il)** \n\n"
   TEXT += f"❂ **Lɪʙʀᴀʀʏ Vᴇʀsɪᴏɴ :** `{telever}` \n\n"
   TEXT += f"❂ **Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
   TEXT += f"❂ **Pʏʀᴏɢʀᴀᴍ Vᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
   TEXT += "** Tʜᴀɴᴋs Fᴏʀ Aᴅᴅɪɴɢ Mᴇ Dᴇᴀʀ🍷**"
-  BUTTON = [[Button.url("Help", f"https://t.me/{BOT_USERNAME}?start=help"), Button.url("• SᴜᴘᴘᴏʀT •", "https://t.me/Lorcan_x_support")]]
+  BUTTON = [[Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/LORCAN_X_UPDATES"), Button.url("• sᴜᴘᴘᴏʀᴛ  •", "https://t.me/Lorcan_x_support")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
