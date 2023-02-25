304bd907aab6e5222ce70d074f51a13184e089b8
Agesilaus <107063122+iAgesilaus@users.noreply.github.com>
2023-02-25 16:49:36 +0530

    Update purge.py

--- LorcanRobot/modules/purge.py
+++ LorcanRobot/modules/purge.py
@@ -21,16 +21,16 @@ async def purge_messages(event):
         )
         and event.from_id not in [1087968824]
     ):
-        await event.reply("Oɴʟʏ Aᴅᴍɪɴs Aʀᴇ Aʟʟᴏᴡᴇᴅ Tᴏ Usᴇ Tʜɪs Cᴏᴍᴍᴀɴᴅ")
+        await event.reply("ᴏɴʟʏ ᴀᴅᴍɪɴ ᴀʀᴇ ᴀʟʟᴏᴡᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ.")
         return
 
     if not await can_delete_messages(message=event):
-        await event.reply("Cᴀɴ'ᴛ Sᴇᴇᴍ Tᴏ Pᴜʀɢᴇ Tʜᴇ Mᴇssᴀɢᴇ")
+        await event.reply("ᴄᴀɴ'ᴛ sᴇᴇᴍ ᴛᴏ ᴘᴜʀɢᴇ ᴛʜᴇ ᴍᴇssᴀɢᴇ.")
         return
 
     reply_msg = await event.get_reply_message()
     if not reply_msg:
-        await event.reply("Rᴇᴘʟʏ Tᴏ A Mᴇssᴀɢᴇ Tᴏ Sᴇʟᴇᴄᴛ Wʜᴇʀᴇ Tᴏ Sᴛᴀʀᴛ Pᴜʀɢɪɴɢ Fʀᴏᴍ.")
+        await event.reply("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ sᴇʟᴇᴄᴛ ᴡʜᴇʀᴇ ᴛᴏ sᴛᴀʀᴛ ᴘᴜʀɢɪɴɢ ғʀᴏᴍ.")
         return
     messages = []
     message_id = reply_msg.id
@@ -71,7 +71,7 @@ async def delete_messages(event):
 
     message = await event.get_reply_message()
     if not message:
-        await event.reply("Wʜᴀᴅʏᴀ Wᴀɴᴛ Tᴏ Dᴇʟᴇᴛᴇ?")
+        await event.reply("Wʜᴀᴅʏᴀ Wᴀɴᴛ Tᴏ Dᴇʟᴇᴛᴇ?") 
         return
     chat = await event.get_input_chat()
     del_message = [message, event.message]
