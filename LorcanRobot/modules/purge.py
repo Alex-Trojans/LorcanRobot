5227ecfa8f12c0ae131c09217d74179caf37d7ea
Agesilaus <107063122+iAgesilaus@users.noreply.github.com>
2023-02-25 16:56:39 +0530

    Update purge.py

--- LorcanRobot/modules/purge.py
+++ LorcanRobot/modules/purge.py
@@ -62,16 +62,16 @@ async def delete_messages(event):
         )
         and event.from_id not in [1087968824]
     ):
-        await event.reply("Oɴʟʏ Aᴅᴍɪɴs Aʀᴇ Aʟʟᴏᴡᴇᴅ Tᴏ Usᴇ Tʜɪs Cᴏᴍᴍᴀɴᴅ")
+        await event.reply("ᴏɴʟʏ ᴀᴅᴍɪɴ ᴀʀᴇ ᴀʟʟᴏᴡᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ.")
         return
 
     if not await can_delete_messages(message=event):
-        await event.reply("Cᴀɴ'ᴛ Sᴇᴇᴍ Tᴏ Dᴇʟᴇᴛᴇ Tʜɪs?")
+        await event.reply("ᴄᴀɴ'ᴛ sᴇᴇᴍ ᴛᴏ ᴅᴇʟᴇᴛ ᴛʜɪs?")
         return
 
     message = await event.get_reply_message()
     if not message:
-        await event.reply("Wʜᴀᴅʏᴀ Wᴀɴᴛ Tᴏ Dᴇʟᴇᴛᴇ?") 
+        await event.reply("ᴡʜᴀᴅʏᴀ ᴡᴀɴᴛ ᴛᴏ ᴅᴇʟᴇᴛ?") 
         return
     chat = await event.get_input_chat()
     del_message = [message, event.message]
